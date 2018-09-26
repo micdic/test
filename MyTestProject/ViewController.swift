508a976b22d5a36bef866257061c65deb05ced83
Michele Di Capua <micheledicapua@10-22-50-15.0200.par.it.iosda.org>
2018-09-26 10:12:00 +0200

    Initial Commit

---
+++ MyTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTestProject
+//
+//  Created by Michele Di Capua on 26/09/18.
+//  Copyright © 2018 Michele Di Capua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
